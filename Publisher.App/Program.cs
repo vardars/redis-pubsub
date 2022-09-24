24dec6dbd384782e40cfd14019e054598e6802db
vardars <vardars1@hotmail.com>
2022-09-24 14:00:14 +0300

    example with FireAndForget publish

---
+++ Publisher.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost");
+
+        ISubscriber subscriber = connectionMultiplexer.GetSubscriber();
+
+        var channel = new RedisChannel("pubsub", RedisChannel.PatternMode.Auto);
+
+        await SchedulePublishMessage(subscriber, channel);
+    }
+
+    private static async Task SchedulePublishMessage(ISubscriber subscriber, RedisChannel channel)
+    {
+        var timer  = new PeriodicTimer(TimeSpan.FromSeconds(5));
+        while (await timer.WaitForNextTickAsync())
+        {
+            subscriber.Publish(channel, $"Time is: {DateTime.Now}", CommandFlags.FireAndForget);
+        }
+    }
+}
---
+++ Subscriber.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost");
+
+        var subscriber = connectionMultiplexer.GetSubscriber();
+
+        var channel = new RedisChannel("pubsub", RedisChannel.PatternMode.Auto);
+
+        subscriber.Subscribe(channel, (channel, message) =>
+        {
+            Console.WriteLine(message);
+        });
+
+        Console.ReadLine();
+    }
+}
